2926a9cb5d80af316ab20492ab56fd67d4be46fb
szmrki <suzumura.kei.x5@elms.hokudai.ac.jp>
2024-11-15 16:08:01 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,214 @@
+#coding: utf-8
+require "sinatra"
+require "sinatra/reloader"
+require "active_record"
+require 'base64'
+require 'sinatra-websocket'
+require 'json'
+require 'yaml'
+
+#セッションを使えるようにする
+enable :sessions
+
+#websocketを使用するとデータベースのリクエストの上限数を超えてしまう
+#ことがあったため、YAMLで設定を記述、上限数を上げた
+db_config = YAML.load_file('config/database.yml')
+ActiveRecord::Base.establish_connection(
+    db_config[settings.environment.to_s]
+)
+
+#websocket用
+set :sockets, []
+
+class User_auth <ActiveRecord::Base
+end
+
+class Paint < ActiveRecord::Base
+end
+
+get '/' do
+    @msg = session[:error]
+    session[:error] = nil
+    erb :index
+end
+
+#描画画面　ログイン、リロード、履歴から戻った際に使用される
+post '/drawing' do
+    if !User_auth.where(:user => params[:user], 
+                        :password => params[:password]).empty?
+        session[:error] = nil 
+        @user = params[:user]
+        session[:user] = @user #session[:~]でセッションが有効となる
+        session[:sid] = session.id #sessionIDを保存
+        erb :drawing
+    else
+        session[:error] = "ログインできませんでした"
+        redirect '/'
+    end        
+end
+
+#描画画面　websocket通信の際に使用される
+get '/drawing' do
+    @title = "Share Paint"
+    @user = session[:user]
+    date = Modify_time()
+    if !request.websocket?
+        halt 404
+    else
+        # WebSocketリクエストを処理
+        request.websocket do |ws|
+            # クライアントが接続したとき
+            ws.onopen do
+            settings.sockets << ws
+            end
+
+            # クライアントから描画データが送信されたとき
+            ws.onmessage do |data|
+                data = JSON.parse(data)
+                #容量削減のためDBにはpathを保存する
+                if data['id'] == -1  #初回の通信
+                    if !Paint.all.empty?
+                        tmp_id = Paint.last.id + 1
+                        id = format("%03d", tmp_id) 
+                    else
+                        tmp_id = 1
+                        id = format("%03d", tmp_id)
+                    end
+                    image_path = "images/paint_#{id}.jpg"
+
+                    #DBに保存する      
+                    Paint.create(:user => @user, 
+                                 :date => date,
+                                 :image_path => image_path)
+
+                else  #2回目以降はidが送られてくる
+                    id = data['id']
+                    updated_data = Paint.find(id)
+                    updated_data.update(:date => date) #更新日時をアップデート
+                    tmp_id = id
+                    id = format("%03d", tmp_id)
+                    image_path = "images/paint_#{id}.jpg"
+                end
+
+                #画像を保存する
+                File.open("./public/#{image_path}", "wb") do |f|
+                    f.write(Base64.decode64(data['image']))
+                end
+                    
+                #画像ファイルのパスを他の全クライアントに送信（ブロードキャスト）
+                settings.sockets.each do |s|
+                    s.send({"image_path" => image_path,
+                            "msg" => "NaN",
+                            "id" => tmp_id}.to_json)
+                end
+            end
+
+            # クライアントが切断したとき
+            ws.onclose do
+                settings.sockets.delete(ws)
+            end
+        end  
+    end
+end
+
+#履歴のページにアクセスする際に使用される
+get '/history' do
+    if !User_auth.where(:user => session[:user]).empty? && 
+                                        session[:sid] == session.id
+        @user = session[:user] 
+        @list = Paint.all
+        erb :history
+    else
+        session[:error] = "ログアウトしました"
+        redirect '/'
+    end
+end
+
+post '/logout' do
+    session.clear  #ログアウトの際はセッションを切断
+    session[:error] = "ログアウトしました"
+    redirect '/'
+end
+
+#全削除ボタンを押した際に使用される
+post '/delete' do
+    Paint.find_by(:image_path=>params[:picture]).destroy
+    File.delete("./public/#{params[:picture]}")
+    redirect '/history'
+end
+
+#保存ボタンを押したときに使用される
+get '/post' do
+    if !request.websocket?
+        halt 404
+    else
+        request.websocket do |ws|
+            ws.onopen do
+                settings.sockets << ws
+            end
+
+            ws.onmessage do |msg|
+                msg = JSON.parse(msg)
+                if msg["msg"] == "reload"
+                    # 全クライアントにリロード命令を送信
+                    settings.sockets.each do |s|
+                        s.send({"image_path" => "NaN",
+                                "msg" => "reload",
+                                "id" => msg["id"]}.to_json)
+                    end                    
+                end
+            end
+
+            ws.onclose do
+                settings.sockets.delete(ws)
+            end
+        end
+    end
+end
+
+#ログイン通知の処理を行うため
+get '/participate' do
+    if !request.websocket?
+        halt 404
+    else
+        request.websocket do |ws|
+            ws.onopen do
+                settings.sockets << ws
+            end
+
+            ws.onmessage do |msg|
+                msg = JSON.parse(msg)
+                if msg["msg"] == "login"
+                    
+                    settings.sockets.each do |s|
+                        #送り主以外の全クライアントにログイン通知
+                        if s != ws
+                            s.send({"image_path" => "NaN",
+                                    "msg" => "login"}.to_json)
+                        else   #送り主にはidと画像のパスを送信
+                            s.send({"image_path" => Paint.last.image_path,
+                                    "msg" => "login",
+                                    "id" => Paint.last.id}.to_json)
+                        end
+                    end
+                end
+            end
+
+            ws.onclose do
+                settings.sockets.delete(ws)
+            end
+        end
+    end
+end
+
+#時間表現を整形するメソッド
+def Modify_time
+    time = Time.now
+    month = format("%02d", time.month)
+    day = format("%02d", time.day)
+    hour = format("%02d", time.hour)
+    min = format("%02d", time.min)
+    sec = format("%02d", time.sec)
+    date = "#{time.year}-#{month}-#{day} #{hour}:#{min}:#{sec} #{time.zone}"
+    return date
+end
---
+++ public/js/drawing.js
@@ -0,0 +1,175 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+let click = 0; //constだと値を再代入できないのでletを使用
+const color = document.getElementById("color");
+const thickness = document.getElementById("thickness");
+const eraser = document.getElementById("eraser");
+const pen = document.getElementById("pen");
+let id = -1;
+const headline = document.getElementById("headline");
+const sessionKey = "first access";
+const sessionValue = true;
+
+White_BG(); //背景色を白に設定
+
+//初回のみlogin_websocket関数を実行
+//sessionstorageにデータがあるかどうかで初回かそうでないか区別
+if (!sessionStorage.getItem(sessionKey)) {
+    login_websocket();
+    sessionStorage.setItem(sessionKey, sessionValue);
+}
+
+id = load_websocket();
+
+//Canvasでマウスを操作した時の処理
+$("#canvas").mousedown(function() {
+    click = 1;
+}).mouseup(function() {
+    click = 0;
+    send_websocket();
+}).mousemove(function(e) {
+    if(!click) return false;
+    draw(e.offsetX, e.offsetY);
+});
+
+//描画を行う関数
+function draw(x, y) {
+    ctx.lineWidth = thickness.value;
+    if (pen.checked) {  //ラジオボタンでペンが選択されていたら
+        ctx.strokeStyle = color.value;
+    }
+    else ctx.strokeStyle = "#FFFFFF";
+    if (click == 1) {
+        click = 2;
+        ctx.beginPath();  //準備
+        ctx.lineCap = "round"; //線を角丸にする
+        ctx.moveTo(x, y);
+    }
+    else {
+        ctx.lineTo(x, y);
+    }
+    ctx.stroke();
+};
+
+//描画の全削除
+$("#clear").click(function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    White_BG();
+    send_websocket();
+});
+
+//画像を保存し、新しいキャンバスに移る
+$("#post").click(function() {
+    const ws_post = new WebSocket('ws://localhost:4567/post');
+    ws_post.onopen = function() {
+        let post_data = {"msg": "reload", "id": id};
+        ws_post.send(JSON.stringify(post_data));
+    }; //reloadという文字列を/postに送る
+      //メッセージを受信した際の処理はload_websocket関数にまとめる(/drawingで処理される)
+    setTimeout(function() {  //すぐにリロードされると通信できなくて困るので遅らせる
+        location.reload(); 
+    }, 1000);
+});
+
+//画像を保存した際の背景色を白にする関数
+function White_BG() {
+    ctx.fillStyle = "#FFF"
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+};
+
+//websocketを通し、最新の画像データを取得する関数
+function load_websocket() {
+    const img = document.createElement('img');
+    const ws = new WebSocket('ws://localhost:4567/drawing');
+    ws.onopen = function() {
+        console.log("open1");
+    };
+    ws.onmessage = function(event) {
+        const image_path = JSON.parse(event.data).image_path; // サーバから画像をロード
+        if (image_path == "NaN") {
+            let return_msg = JSON.parse(event.data).msg;
+            if (return_msg == "reload") {
+                location.reload();
+            }
+            /*他ユーザがログインした時の処理
+            画像データとidを送ってあげる*/
+            else if (return_msg == "login") {  
+                console.log("他のユーザがログインしました");
+                let base64 = canvas.toDataURL("image/jpeg");
+                let sync_data = {"image": base64.replace(/^.*,/, ''), "id": id};
+                setTimeout(function() {
+                    ws.send(JSON.stringify(sync_data));
+                }, 100);
+            }
+            else {
+                id = JSON.parse(event.data).id; //idを取得
+                setTimeout(function() {
+                    ws.close();
+                }, 100);
+            }
+        }
+        else {
+            id = JSON.parse(event.data).id; //idを取得
+            headline.innerHTML = `Canvas<br>Number:${id}`;
+            img.src = image_path + "?t=" + new Date().getTime(); 
+            //同じパスだとキャッシュからデータがとられるため、画像が更新されない
+            //そこで、現在時刻をクエリに含めることでキャッシュからとられず、常にサーバから画像を取得するようにした
+            //img.src = image_path
+            img.onload =  function() {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                White_BG();
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            };
+            console.log(event.data);
+            setTimeout( ()=> {
+                    ws.close();
+            }, 100);
+        }
+    };
+    ws.onerror = function() {
+        console.log("error1");
+    };
+    ws.onclose = function() {
+        console.log("close1");
+        setTimeout(load_websocket(), 1000); //再帰呼び出しにすることで、１回の通信で終わらせないようにする
+    };
+    return id //別ブラウザで描いたものとidを合わせるため
+};
+
+//websocketを通し、画像データを送信する関数
+function send_websocket() {
+    let base64 = canvas.toDataURL("image/jpeg");
+    let data = {"image": base64.replace(/^.*,/, ''), "id": id};
+    const ws = new WebSocket('ws://localhost:4567/drawing');
+    ws.onopen = function() {
+        ws.send(JSON.stringify(data));
+    };
+    ws.onmessage = function(data) {
+        id = JSON.parse(data.data).id;
+        console.log(id)
+        headline.innerHTML = `Canvas<br>Number:${id}`;
+        setTimeout( ()=> {
+            ws.close();
+        }, 100);
+    };
+    ws.onerror = function() {
+        console.log("error2");
+    };
+};
+
+//ログインしたことを他のユーザに知らせる。この通信をきっかけとして、キャンバスを同期させる
+function login_websocket() {
+    const ws_login = new WebSocket('ws://localhost:4567/participate');
+    ws_login.onopen = function() {
+        let login_data = {"msg": "login"};
+        ws_login.send(JSON.stringify(login_data));
+    };
+    ws_login.onmessage = function() {
+        setTimeout(function() {
+            ws_login.close();
+        }, 100);
+    };
+    ws_login.onerror = function() {
+        console.log("error3");
+    };
+};
